a87a562b5eae2fc53e8d2a6b72ea5aafe54aa603
angelhpascual <angelhpascual@gmail.com>
2023-01-21 01:24:37 +0100

    interface created and added a h1 and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import "./App.css"
+import React, { useState } from "react"
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
   return (
-    <div className="App">
-      <p className="text-xl">Hello World!</p>
+    <div className="h-screen bg-indigo-500 flex flex-col justify-center items-center">
+      <div className="text-xl font-bold text-indigo-100 mb-8">TIC TAC TOE</div>
+      <div className="grid grid-cols-3 gap-2 p-2">
+        {board.map((value, index) => {
+          return (
+            <div className="w-16 h-16 border-2 bg-indigo-600" key={index}>
+              {value}
+            </div>
+          )
+        })}
+      </div>
+      <footer className="mt-8">
+        <p className="text-indigo-100 text-sm">
+          Developed by <span className="underline">angelhpascual</span>
+        </p>
+      </footer>
     </div>
   )
 }

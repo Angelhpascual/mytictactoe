247f270380689acdbcf4ae726c9cac23b0cf03a5
angelhpascual <angelhpascual@gmail.com>
2023-01-21 01:33:30 +0100

    added some styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   return (
     <div className="h-screen bg-indigo-500 flex flex-col justify-center items-center">
-      <div className="text-xl font-bold text-indigo-100 mb-8">TIC TAC TOE</div>
+      <p className="text-xl font-bold text-indigo-200 mb-8 border-2 border-indigo-200 py-2 px-4">
+        TIC TAC TOE
+      </p>
       <div className="grid grid-cols-3 gap-2 p-2">
         {board.map((value, index) => {
           return (
@@ -13,6 +15,10 @@ function App() {
             </div>
           )
         })}
+      </div>
+      <div className="flex items-center justify-center mt-4 gap-2 text-2xl text-white">
+        <div className="bg-indigo-600 py-2 px-4 cursor-pointer">X</div>
+        <div className="bg-indigo-600 py-2 px-4 cursor-pointer">O</div>
       </div>
       <footer className="mt-8">
         <p className="text-indigo-100 text-sm">
